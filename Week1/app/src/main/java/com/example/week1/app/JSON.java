c19fdf2342c5c6d7238cbf391819bef5e43d2c7b
Robert Smith <r2theob23@gmail.com>
2014-06-03 20:42:34 -0500

    Added JSON class that parses the JSON as required

---
+++ Week1/app/src/main/java/com/example/week1/app/JSON.java
@@ -0,0 +1,70 @@
+package com.example.week1.app;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by robertsmith on 6/3/14.
+ */
+public class JSON {
+
+    public static final String TAG = JSON.class.getSimpleName();
+    public static String[] boxOfficeMovies = new String[10];
+    public static String[] rentalMovies = new String[10];
+
+
+
+    public static String[] getBoxOfficeJSON(String s)
+    {
+        try
+        {
+            JSONObject jsonResponse = new JSONObject(s);
+            JSONArray movies = jsonResponse.getJSONArray("movies");
+            Log.e("TAG", "Movies: " + movies);
+
+            for (int i = 0; i < movies.length(); i++)
+            {
+                JSONObject movie = movies.getJSONObject(i);
+                String title = movie.getString("title");
+                String rating = movie.getString("mpaa_rating");
+
+                boxOfficeMovies[i] = title;
+
+            }
+        }
+        catch (JSONException e)
+        {
+            Log.e("TAG", "ERROR: " + e);
+        }
+
+        return boxOfficeMovies;
+    }
+
+    public static String[] getRentalJSON(String s)
+    {
+        try
+        {
+            JSONObject jsonResponse = new JSONObject(s);
+            JSONArray movies = jsonResponse.getJSONArray("movies");
+            Log.e("TAG", "Movies: " + movies);
+
+            for (int i = 0; i < movies.length(); i++)
+            {
+                JSONObject movie = movies.getJSONObject(i);
+                String title = movie.getString("title");
+                String rating = movie.getString("mpaa_rating");
+
+                rentalMovies[i] = title;
+            }
+        }
+        catch (JSONException e)
+        {
+            Log.e("TAG", "ERROR: " + e);
+        }
+
+        return rentalMovies;
+    }
+}
